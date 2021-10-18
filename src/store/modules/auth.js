039661cfd984ce522b094bcb5766f97e07c381d0
AlexandrKuleshov <alexandr.kuleshov@leetio.org>
2021-10-18 17:45:17 +0300

    auth

---
+++ src/store/modules/auth.js
@@ -0,0 +1,31 @@
+import authApi from '@/api/auth'
+
+const state = {
+  isSubmitting: false
+}
+
+const mutations = {
+  registerStart(state) {
+    state.isSubmitting = true
+  }
+}
+
+const actions = {
+  register(context, credentials) {
+    return new Promise(() => {
+      authApi.register(credentials)
+        .then(response => {
+          console.log('response', response)
+        })
+        .catch(result => {
+          console.log('result errors', result)
+        })
+    })
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
